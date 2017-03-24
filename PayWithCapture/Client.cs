24bc071dd4de1ae890bbff83e1f17d778c5b3e1f
Ikechi Michael <mykehell123@gmail.com>
2017-03-24 02:19:13 +0100

    created authenticate method in Client

--- PayWithCapture/Client.cs
+++ PayWithCapture/Client.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PayWithCapture.Models;
+using PayWithCapture.Helpers;
+using PayWithCapture.Common;
 
 namespace PayWithCapture
 {
     public class Client
     {
+        private AuthenticationRequest _config { get; set; }
+        private AuthenticationResponse _tokenInfo { get; set; }
+        public Client(string clientId, string clientSecret)
+        {
+            this._config = new AuthenticationRequest()
+            {
+                client_id = clientId,
+                client_secret = clientSecret
+            };
+        }
 
+        public Client(AuthenticationRequest config)
+        {
+            if (config == null) throw new Exception("Config should not be NULL abeg");
+            this._config = config;
+        }
+
+        public async Task<AuthenticationResponse> authenticate()
+        {
+            return this._tokenInfo = await Api.GetAsync<AuthenticationResponse>(Constants.ResolveApiUrl(Constants.OAUTH_TOKEN_URL));
+        }
     }
 }
--- PayWithCapture/Common/Constants.cs
+++ PayWithCapture/Common/Constants.cs
@@ -8,6 +8,17 @@ namespace PayWithCapture.Common
 {
     public class Constants
     {
+        public const string CONTENT_TYPE_XML = "text/xml";
+        public const string CONTENT_TYPE_JSON = "application/json";
         public const string GRANT_TYPE = "client_credentials";
+        public const string SERVER_BASE_URL_STAGING = "https://pwcstaging.herokuapp.com/";
+        public const string SERVER_BASE_URL_PRODUCTION = "https://pwcstaging.herokuapp.com/";
+        public const string OAUTH_TOKEN_URL = "oauth/token";
+        public static Environment CurrentEnvironment { get; set; }
+
+        public static string ResolveApiUrl(string urlSuffix)
+        {
+            return $"{(CurrentEnvironment == Environment.Staging ? SERVER_BASE_URL_STAGING : SERVER_BASE_URL_PRODUCTION)}{urlSuffix}";
+        }
     }
 }
